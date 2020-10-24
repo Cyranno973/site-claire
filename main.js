f27c05df8ccb84be2a3ad336651c4f649b0998ba
Cyranno973 <gs.gouin@gmail.com>
2020-10-25 01:31:45 +0200

    date gérer

--- main.js
+++ main.js
@@ -1,16 +1,49 @@
 
-let dateToday1 = new Date();
-let date = new Date(dateToday1);
-console.log(date);
+let dateToday = new Date("2020-11-10");
+let date = new Date(dateToday);
+let dateJour = date.getDay();
+let dateFomat = `${date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear()}`;
 
-let dateNow = `${date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear()}`;
+let newdate = new Date(dateToday);
+//console.log('newdate avant :', newdate);
 
-console.log(dateNow);
+let whatDay = dateJour === 4 ? (0) : (4-dateJour );
+console.log('whatDay', whatDay);
+
+
+newdate.setDate(dateToday.getDate()+whatDay);
+console.log('newdate  apres :', newdate);
+
+//whatDay = dateJour < 4 ? (4-dateJour) : dateJour > 4  ? (4-dateJour ) : 0;
+
+dateFomat = `${newdate.getDate()+"/"+(newdate.getMonth()+1)+"/"+newdate.getFullYear()}`;
+// esxemplet on est mardi 
+//    0       1   2       3       4       5       6
+//dimanche lundi mardi mercredi jeudi vendredi samedi 
+
+// 4-2 = 2 
+//date du jour +2 
+
+
+
+
+
+
+
+
+
+
+
+
+
+//console.log('dateFomat', dateFomat);
+// let lol ;
+let itemTotal = document.querySelectorAll('.container-permanence-content > .content-tables');
 btnPlus = document.getElementById('plus');
 
 let item = `  
 <div class="container-permanence-content">
-<div class="container-dates content-tables">Jeudi ${dateNow}</div>
+<div contenteditable="true" class="container-dates content-tables">Jeudi ${dateFomat}</div>
 <div class="container-hours content-tables">13h30 - 14h15</div>
 <div contenteditable="true" class="container-names content-tables"></div>
 <div contenteditable="true" class="container-phones content-tables"></div>
@@ -19,17 +52,102 @@ let item = `
 <div contenteditable="true" class="container-traitements content-tables"></div>
 </div>
 `;
+// let i = 0;
 
 
 btnPlus.addEventListener('click', function(){
-    if(date.getDay() !== 4){
-        document.querySelector('.container-body').innerHTML += item;
-    }else{
-    
-    }
-    
-});   
+    document.getElementById('container-body').innerHTML += item;
 
-let itemTotal = document.querySelectorAll('.container-permanence-content > .content-tables');
+  //  if(dateJour === 4){
+        
+   // }//else{
+      // newdate.setDate(date.getDay()+1);
+        
+       // console.log(newdate.setDate(date.getDay()+1));
+        
+     //while (lol !== 4) {
 
-console.log(itemTotal);
+       // lol = newdate.setDate(date.getDay()+1);
+      //  console.log('newdate', newdate);
+                
+                  
+                 
+       // }
+        // while (i < 20000) {
+        //          i++;
+        //           console.log('i', i);
+        //          lol = newdate.setDate(date.getDay()+1);
+        //           console.log('newdate', newdate);
+        //         setTimeout(function(){
+        //             i=20000;
+        //             console.log(i+'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy')},1);
+        //             return null;
+                  
+                 
+        // }
+    //}
+}); 
+
+// while (i < 5) {
+//     i++;
+//      console.log('i', i);
+     
+// }
+
+//buildTable(myData);
+
+
+
+// while (date.getDay() !== 4) {
+    
+// }
+
+// function goThursday (dateToday){
+//     console.log('dateToday', dateToday);
+//     let newdate = new Date(dateToday);
+//     console.log('newdate', newdate);
+//     newdate.setDate(newdate.getDay()+1);
+//     return newdate;
+// }
+
+// console.log(goThursday(dateToday));
+
+// var myData = JSON.parse(data);
+
+// console.log('myData', myData);
+
+
+// let titre = document.querySelector('.container-title');
+
+// titre.textContent = myData[0].name;
+
+// myData[0].name = 'John';
+// titre.textContent = myData[0].name;
+// console.log(typeof myData);
+
+// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+// console.log(words);
+// const result = words.filter(mot => mot.length >6 );
+// console.log(result);
+
+// function suffisamentGrand()
+// function buildTable(data) {
+//     var table = document.getElementById('container-body');
+//     console.log('table', table);
+
+    
+//     for (let i = 0; i < data.length; i++) {
+//         let row =`<div class="container-permanence-content">
+//         <div contenteditable="true" class="container-dates content-tables">Jeudi ${dateFomat}</div>
+//         <div class="container-hours content-tables">13h30 - 14h15</div>
+//         <div contenteditable="true" class="container-names content-tables"> ${data[i].name}</div>
+//         <div contenteditable="true" class="container-phones content-tables"></div>
+//         <div contenteditable="true" class="container-presences content-tables"></div>
+//         <div contenteditable="true" class="container-asts content-tables"></div>
+//         <div contenteditable="true" class="container-traitements content-tables"></div>
+//         </div>`;
+//         table.innerHTML+= row;
+        
+//     }
+// }
+// buildTable(myData);
