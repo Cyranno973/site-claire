0beb1ebcf5dc13311fa83954aed1252dfe2d66d7
Cyranno973 <gs.gouin@gmail.com>
2020-10-29 02:17:43 +0100

    enregistrement firebase effectuer

---
+++ main copy.js
@@ -0,0 +1,187 @@
+
+
+
+//  const userListUI = document.getElementById("lol");
+//  usersRef.on("child_added", snap => {
+//      let user = snap.val();
+//      let $li = document.createElement("li");
+//      $li.innerHTML = user.name;
+//      $li.setAttribute("child-key", snap.key);
+//      $li.addEventListener("click", userClicked) userListUI.append($li);
+//  });
+//  function userClicked(e) {
+//   var userID = e.target.getAttribute("child-key");
+//   const userRef = dbRef.child('users/' + userID);
+//   const userDetailUI = document.getElementById("userDetail");
+//   userDetailUI.innerHTML = ""
+//   userRef.on("child_added", snap => {
+//       var $p = document.createElement("p");
+//       $p.innerHTML = snap.key + " - " + snap.val() userDetailUI.append($p);
+//   });
+// }
+
+
+
+
+
+
+
+
+
+let dateToday = new Date();
+let date = new Date(dateToday);
+let dateJour = date.getDay();
+let dateFomat = `${date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear()}`;
+
+let newdate = new Date(dateToday);
+//console.log('newdate avant :', newdate);
+
+let whatDay = dateJour === 4 ? (0) : (4-dateJour );
+console.log('whatDay', whatDay);
+
+
+newdate.setDate(dateToday.getDate()+whatDay);
+console.log('newdate  apres :', newdate);
+
+//whatDay = dateJour < 4 ? (4-dateJour) : dateJour > 4  ? (4-dateJour ) : 0;
+
+dateFomat = `${newdate.getDate()+"/"+(newdate.getMonth()+1)+"/"+newdate.getFullYear()}`;
+// esxemplet on est mardi 
+//    0       1   2       3       4       5       6
+//dimanche lundi mardi mercredi jeudi vendredi samedi 
+
+// 4-2 = 2 
+//date du jour +2 
+
+
+
+
+var now = new Date();
+now.setMinutes(now.getMinutes() + 30); // timestamp
+now = new Date(now); // Date object
+console.log(now);
+
+
+
+
+
+
+
+
+//console.log('dateFomat', dateFomat);
+// let lol ;
+let itemTotal = document.querySelectorAll('.container-permanence-content > .content-tables');
+btnPlus = document.getElementById('container-plus');
+
+let item = `  
+<div class="container-permanence-content">
+<div contenteditable="true" class="container-dates content-tables">Jeudi ${dateFomat}</div>
+<div class="container-hours content-tables">13h30 - 14h15</div>
+<div contenteditable="true" class="container-names content-tables"></div>
+<div contenteditable="true" class="container-phones content-tables"></div>
+<div contenteditable="true" class="container-presences content-tables"></div>
+<div contenteditable="true" class="container-asts content-tables"></div>
+<div contenteditable="true" class="container-traitements content-tables"></div>
+</div>
+`;
+// let i = 0;
+
+
+btnPlus.addEventListener('click', function(event){
+  console.log('lol');
+  document.getElementById('container-body').innerHTML += item;
+  event.stopPropagation();
+
+  //  if(dateJour === 4){
+        
+   // }//else{
+      // newdate.setDate(date.getDay()+1);
+        
+       // console.log(newdate.setDate(date.getDay()+1));
+        
+     //while (lol !== 4) {
+
+       // lol = newdate.setDate(date.getDay()+1);
+      //  console.log('newdate', newdate);
+                
+                  
+                 
+       // }
+        // while (i < 20000) {
+        //          i++;
+        //           console.log('i', i);
+        //          lol = newdate.setDate(date.getDay()+1);
+        //           console.log('newdate', newdate);
+        //         setTimeout(function(){
+        //             i=20000;
+        //             console.log(i+'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy')},1);
+        //             return null;
+                  
+                 
+        // }
+    //}
+}); 
+
+// while (i < 5) {
+//     i++;
+//      console.log('i', i);
+     
+// }
+
+//buildTable(myData);
+
+
+
+// while (date.getDay() !== 4) {
+    
+// }
+
+// function goThursday (dateToday){
+//     console.log('dateToday', dateToday);
+//     let newdate = new Date(dateToday);
+//     console.log('newdate', newdate);
+//     newdate.setDate(newdate.getDay()+1);
+//     return newdate;
+// }
+
+// console.log(goThursday(dateToday));
+
+// var myData = JSON.parse(data);
+
+// console.log('myData', myData);
+
+
+// let titre = document.querySelector('.container-title');
+
+// titre.textContent = myData[0].name;
+
+// myData[0].name = 'John';
+// titre.textContent = myData[0].name;
+// console.log(typeof myData);
+
+// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+// console.log(words);
+// const result = words.filter(mot => mot.length >6 );
+// console.log(result);
+
+// function suffisamentGrand()
+// function buildTable(data) {
+//     var table = document.getElementById('container-body');
+//     console.log('table', table);
+
+    
+//     for (let i = 0; i < data.length; i++) {
+//         let row =`<div class="container-permanence-content">
+//         <div contenteditable="true" class="container-dates content-tables">Jeudi ${dateFomat}</div>
+//         <div class="container-hours content-tables">13h30 - 14h15</div>
+//         <div contenteditable="true" class="container-names content-tables"> ${data[i].name}</div>
+//         <div contenteditable="true" class="container-phones content-tables"></div>
+//         <div contenteditable="true" class="container-presences content-tables"></div>
+//         <div contenteditable="true" class="container-asts content-tables"></div>
+//         <div contenteditable="true" class="container-traitements content-tables"></div>
+//         </div>`;
+//         table.innerHTML+= row;
+        
+//     }
+// }
+// buildTable(myData);
--- main.js
+++ main.js
@@ -1,156 +1,293 @@
 
+// var database = firebase.database();
+// console.log(database);
+  //  let dbRef = firebase.database().ref('/messages');
+   let dbRef = firebase.database().ref();
+   let dbRef1 = firebase.database().ref('users');
+   let bodytable = document.getElementById('container-body');
+   let valider = document.getElementById('valider');
 let dateToday = new Date();
 let date = new Date(dateToday);
 let dateJour = date.getDay();
 let dateFomat = `${date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear()}`;
+let id = idRandom = getRandomInt(30000);
 
-let newdate = new Date(dateToday);
-//console.log('newdate avant :', newdate);
+// let dateFomat2 = `${date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear()}`;
+//   console.log('dbRef', dbRef);
+//  const usersRef = dbRef.child('users/data');
+//  const usersRef = dbRef.child('object');
+// console.log('usersRef', usersRef);
+ //usersRef.push('blabla')
 
-let whatDay = dateJour === 4 ? (0) : (4-dateJour );
-console.log('whatDay', whatDay);
+//  dbRef.on('child_added', function(snapshot){console.log(snapshot.key)});
 
+//////////////////////////////////////////      Id randomm ////////////////////////
 
-newdate.setDate(dateToday.getDate()+whatDay);
-console.log('newdate  apres :', newdate);
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
-//whatDay = dateJour < 4 ? (4-dateJour) : dateJour > 4  ? (4-dateJour ) : 0;
-
-dateFomat = `${newdate.getDate()+"/"+(newdate.getMonth()+1)+"/"+newdate.getFullYear()}`;
-// esxemplet on est mardi 
-//    0       1   2       3       4       5       6
-//dimanche lundi mardi mercredi jeudi vendredi samedi 
-
-// 4-2 = 2 
-//date du jour +2 
-
+idRandom = getRandomInt(30000);
+console.log(idRandom);
 
 
-
-var now = new Date();
-now.setMinutes(now.getMinutes() + 30); // timestamp
-now = new Date(now); // Date object
-console.log(now);
+///////////////////// demande d'ecoute sur la base de donner et lancement de la fonction messageAdded  ///////////////////////////
+dbRef1.on('child_added', messageAdded);
 
 
+///////////////////////////////////////////// Parcourir les resultat d'un Id   /////////////////////////////////////////////////////
 
 
+//  dbRef1.on("value", function(snapshot) {
+  // results = snapshot.val();
+  // console.log(results);
 
-
-
-
-//console.log('dateFomat', dateFomat);
-// let lol ;
-let itemTotal = document.querySelectorAll('.container-permanence-content > .content-tables');
-btnPlus = document.getElementById('plus');
-
-let item = `  
-<div class="container-permanence-content">
-<div contenteditable="true" class="container-dates content-tables">Jeudi ${dateFomat}</div>
-<div class="container-hours content-tables">13h30 - 14h15</div>
-<div contenteditable="true" class="container-names content-tables"></div>
-<div contenteditable="true" class="container-phones content-tables"></div>
-<div contenteditable="true" class="container-presences content-tables"></div>
-<div contenteditable="true" class="container-asts content-tables"></div>
-<div contenteditable="true" class="container-traitements content-tables"></div>
-</div>
-`;
-// let i = 0;
-
-
-btnPlus.addEventListener('click', function(){
-    document.getElementById('container-body').innerHTML += item;
-
-  //  if(dateJour === 4){
-        
-   // }//else{
-      // newdate.setDate(date.getDay()+1);
-        
-       // console.log(newdate.setDate(date.getDay()+1));
-        
-     //while (lol !== 4) {
-
-       // lol = newdate.setDate(date.getDay()+1);
-      //  console.log('newdate', newdate);
-                
-                  
-                 
-       // }
-        // while (i < 20000) {
-        //          i++;
-        //           console.log('i', i);
-        //          lol = newdate.setDate(date.getDay()+1);
-        //           console.log('newdate', newdate);
-        //         setTimeout(function(){
-        //             i=20000;
-        //             console.log(i+'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy')},1);
-        //             return null;
-                  
-                 
-        // }
-    //}
-}); 
-
-// while (i < 5) {
-//     i++;
-//      console.log('i', i);
-     
-// }
-
-//buildTable(myData);
-
-
-
-// while (date.getDay() !== 4) {
+// for (const key in results) {
+//   if (results.hasOwnProperty(key)) {
+//     const element = results[key];
+//     // console.log(element);
     
+//   }
 // }
 
-// function goThursday (dateToday){
-//     console.log('dateToday', dateToday);
-//     let newdate = new Date(dateToday);
-//     console.log('newdate', newdate);
-//     newdate.setDate(newdate.getDay()+1);
-//     return newdate;
-// }
-
-// console.log(goThursday(dateToday));
-
-// var myData = JSON.parse(data);
-
-// console.log('myData', myData);
+//  })
 
 
-// let titre = document.querySelector('.container-title');
 
-// titre.textContent = myData[0].name;
+  //  dbRef.on('child_added', messageAdded);
 
-// myData[0].name = 'John';
-// titre.textContent = myData[0].name;
-// console.log(typeof myData);
+  
+function messageAdded(snap) {
+   console.log(snap.val());
+  addItem({
+    // id: snap.key,
+    id: snap.val().id,
+    daten: snap.val().daten,
+    heure: snap.val().heure,
+    names: snap.val().names,
+    tel:  snap.val().telo,
+    prescence: snap.val().prescence,
+    assist: snap.val().assist,
+    traiter: snap.val().traiter
+  })
+}
 
-// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
-// console.log(words);
-// const result = words.filter(mot => mot.length >6 );
-// console.log(result);
 
-// function suffisamentGrand()
-// function buildTable(data) {
-//     var table = document.getElementById('container-body');
-//     console.log('table', table);
+function addItem(data) {
+  console.log(data);
+  // let message = document.getElementById('message');
+  
+    let item = document.getElementById('template').cloneNode(true);
+    // console.log('item', item);
+    item.setAttribute('id', data.id);
 
-    
-//     for (let i = 0; i < data.length; i++) {
-//         let row =`<div class="container-permanence-content">
-//         <div contenteditable="true" class="container-dates content-tables">Jeudi ${dateFomat}</div>
-//         <div class="container-hours content-tables">13h30 - 14h15</div>
-//         <div contenteditable="true" class="container-names content-tables"> ${data[i].name}</div>
-//         <div contenteditable="true" class="container-phones content-tables"></div>
-//         <div contenteditable="true" class="container-presences content-tables"></div>
-//         <div contenteditable="true" class="container-asts content-tables"></div>
-//         <div contenteditable="true" class="container-traitements content-tables"></div>
-//         </div>`;
-//         table.innerHTML+= row;
+
+    item.querySelector('.date').textContent = data.daten;
+    item.querySelector('.heure').textContent = data.heure;
+    item.querySelector('.names-user').textContent = data.names;
+    item.querySelector('.tel').textContent = data.tel;
+    item.querySelector('.prescence').textContent = data.prescence;
+    item.querySelector('.assist').textContent = data.assist;
+    item.querySelector('.traiter').textContent = data.traiter;
+    // console.log('item', item);
+  
+    document.addEventListener('keydown', function(e){
+      if (e.code === 'Enter') {
         
-//     }
+        // console.log(e.code);
+      }
+    });
+    
+    bodytable.appendChild(item);
+    
+  }
+  function subItem(data) {
+    // console.log(data.id);
+    // console.log(data.daten);
+    // console.log(data.heure);
+    // console.log(data.names);
+    // console.log(data.telo);
+    // console.log(data.prescence);
+    // console.log(data.assist);
+    // console.log(data.traiter);
+    // let message = document.getElementById('message');
+    
+      let item = document.getElementById('template').cloneNode(true);
+      // console.log('item', item);
+      item.setAttribute('id', data.id);
+      item.querySelector('.date').textContent = data.daten;
+      item.querySelector('.heure').textContent = data.heure;
+      item.querySelector('.names-user').textContent = data.names;
+      item.querySelector('.tel').textContent = data.telo;
+      item.querySelector('.prescence').textContent = data.prescence;
+      item.querySelector('.assist').textContent = data.assist;
+      item.querySelector('.traiter').textContent = data.traiter;
+      // console.log('item', item);
+
+      bodytable.appendChild(item);
+      
+    }
+  
+///////////////////////// valider //////////////////////////////////////
+valider.addEventListener('click', function(e){
+  e.preventDefault();
+
+  addToFirebase();
+
+});
+
+
+function addToFirebase(e) {
+let datead = document.getElementById('date').value;
+  // console.log('datead', datead);
+let heuread = document.getElementById('heure').value;
+// console.log('heuread', heuread);
+let namesad = document.getElementById('names-user').value;
+// console.log('namesad', namesad);
+let telad = document.getElementById('tel').value;
+// console.log('telad', telad);
+let prescencead = document.getElementById('prescence').value;
+// console.log('prescencead', prescencead);
+let assistad = document.getElementById('assist').value;
+// console.log('assistad', assistad);
+let traiterad = document.getElementById('traiter').value;
+// console.log('traiterad', traiterad);
+
+dbRef1.push({
+    id: idRandom,
+    daten: datead,
+    heure: heuread,
+    names: namesad,
+    telo: telad,
+    prescence: prescencead,
+    assist: assistad,
+    traiter: traiterad,
+  })
+}
+
+
+
+
+// let tel = document.querySelector('.tel').textContent;
+
+
+
+  // function addToFirebase(e) {
+  //   e.preventDefault();
+  //   dbRef.push({
+  //     message: message.value,
+  //     message1: message.value,
+  //   })
+  //   message.value='';
+  //   message1.value='';
+  //   message.focus();
+  //   message1.focus();
+  // }
+
+
+
+// function addToFirebase(e) {
+//   e.preventDefault();
+//   usersRef.push({
+//     message: message.value,
+//   })
+//   message.value='';
+//   message.focus();
 // }
-// buildTable(myData);
+// function addToFirebase(e) {
+//   e.preventDefault();
+//   dbRef.push({
+//     message: message.value,
+//     message1: message.value,
+//   })
+//   message.value='';
+//   message1.value='';
+//   message.focus();
+//   message1.focus();
+// }
+
+
+
+// let dateus = new Date();
+// let dateusto = new Date(dateus);
+// let horaire = '13h30';
+// let phone =0140568545;
+// let assist =8545;
+// let nameUser = 'Bertrand Cammille';
+// let prescence = 'O';
+// let traiter = 'Y';
+// let id2 = 2;
+// let dateus2 = new Date();
+// let dateusto2 = new Date(dateus);
+// let horaire2 = '13h30';
+// let phone2 =0140568545;
+// let assist2 =8545;
+// let nameUser2 = 'Bertrand Cammille';
+// let prescence2 = 'O';
+// let traiter2 = 'Y';
+
+// writeUserData(id,dateFomat,horaire,nameUser,phone,prescence,assist,tel);
+// writeUserData(id2,dateFomat2,horaire2,nameUser2,phone2,prescence2,assist2,tel);
+
+function writeUserData(userId, dateus, heure, nameUser,tel,prescence,assist,traiter) {
+  firebase.database().ref('users/' + userId).set({
+    daten: dateFomat,
+    heure: horaire,
+    names: nameUser,
+    tel: phone,
+    prescence: prescence,
+    assist: assist,
+    traiter: traiter
+  });
+}
+
+
+
+
+
+
+
+
+// usersRef.on('value', function(snapshot){console.log(snapshot)})
+// usersRef.on('value', function(snapshot){console.log(snapshot.val())})
+// usersRef.on('child_added', function(snapshot){console.log(snapshot.val())})
+// usersRef.on('child_added', function(snapshot){console.log(snapshot.key())})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  const userListUI = document.getElementById("lol");
+//  usersRef.on("child_added", snap => {
+//      let user = snap.val();
+//      let $li = document.createElement("li");
+//      $li.innerHTML = user.name;
+//      $li.setAttribute("child-key", snap.key);
+//      $li.addEventListener("click", userClicked) userListUI.append($li);
+//  });
+//  function userClicked(e) {
+//   var userID = e.target.getAttribute("child-key");
+//   const userRef = dbRef.child('users/' + userID);
+//   const userDetailUI = document.getElementById("userDetail");
+//   userDetailUI.innerHTML = ""
+//   userRef.on("child_added", snap => {
+//       var $p = document.createElement("p");
+//       $p.innerHTML = snap.key + " - " + snap.val() userDetailUI.append($p);
+//   });
+// }
+
