3fbf5cd69c8195f7b53d39b725fc9953b1d11ccf
Cyranno973 <gs.gouin@gmail.com>
2020-12-01 01:55:27 +0100

    premier jour de debut de comprehension du code je respire enfin je vois le bout du tunnel je visualise L'html
    JE vois donc je suis .

--- js/function.js
+++ js/function.js
@@ -1,96 +1,73 @@
 // La fonction read permet la lecture de la base.
 function read(type, annee, mois, jour = "", idRdv = "") {
-  const uri = `App/${type}/${annee}/${mois}/${jour}/${idRdv}`;
-  return dbRef.child(uri).once("value");
+    const uri = `App/${type}/${annee}/${mois}/${jour}/${idRdv}`;
+    return dbRef.child(uri).once("value");
 }
 
 // function update permet la mise a jours de donnee
 function update(type, annee, mois, jour, idRdv, rdv) {
-  const uri = `App/${type}/${annee}/${mois}/${jour}/${idRdv}`;
+    const uri = `App/${type}/${annee}/${mois}/${jour}/${idRdv}`;
 
-  read(type, annee, mois, jour, idRdv).then((snap) => {
-    const rdvOld = snap.val();
-    const newVal = { ...rdvOld, ...rdv };
+    read(type, annee, mois, jour, idRdv).then((snap) => {
+        const rdvOld = snap.val();
+        const newVal = {...rdvOld, ...rdv};
 
-    dbRef
-      .child(uri)
-      .set(newVal)
-      .then(() => console.log("success"))
-      .catch((error) => console.log("failed ", error));
-  });
+        dbRef
+            .child(uri)
+            .set(newVal)
+            .then(() => console.log("success"))
+            .catch((error) => console.log("failed ", error));
+    });
 }
 
-// function addTODo(){
-    
-// }
+// Mappe l'objet retourné par firebase pour que ça corresponde la structure du front 
+function adapter(snap, annee, mois) {
+    // console.log(snap);
+    const jours = [];
+    if (snap) {
+        const snapObjet = Object.entries(snap);
 
+        snapObjet.map(jourKeyValue => {
+            // Créer les rdvs
+            const rdvs = [];
 
+            // Ajouter les rdv
+            Object.entries(jourKeyValue[1]).map(rdvKeyValue => {
+                let nouveauRdv = new Rdv();
+                nouveauRdv.debut = rdvKeyValue[1].debut;
+                nouveauRdv.fin = rdvKeyValue[1].fin;
+                nouveauRdv.names = rdvKeyValue[1].names;
+                nouveauRdv.ast = rdvKeyValue[1].ast;
+                nouveauRdv.presence = rdvKeyValue[1].presence;
+                nouveauRdv.commentaire = rdvKeyValue[1].commentaire;
+                nouveauRdv.traite = rdvKeyValue[1].traite;
+                nouveauRdv.tel = rdvKeyValue[1].tel;
+                //ou spread
+                // nouveauRdv =  {id:rdvKeyValue[0],...rdvKeyValue[1]};
+                rdvs.push(nouveauRdv);
+            });
 
-// function snapToApp(snap) {
-//     const jours = [];
-//     snap.map(snap => {
-//         const rdvs = jour.rdv.map(rdv => {
+            // Créer un jour
+            const newJour = new Jour(jourKeyValue[0], rdvs);
+            // Ajouter le jour à la liste des jours
+            jours.push(newJour);
+        });
+    }
 
-//         });
-//         const jour = new Jour();
-//         // Créer les rdvs
-//         // Créer un jour
-//         // Ajouter les rdv
-//         // Ajouter le jour à la liste des jours
-//         jours.push(jour);
-//     });
-//     return jours;
-// }
+    const m1 = new Mois(mois, jours);
+    return new Annee(annee, [m1]); // a modifier poujr afficher plusieurs mois
+}
 
-// class Rdv {
-//     constructor(
-//         heureDebut,
-//         heureFin,
-//         nomPrenom,
-//         Assist,
-//         Presence,
-//         traite) {
-//     }
-// }
-
-// class Jour {
-//     constructor(date, rdvs) {
-//     }
-// }
-
-// class Rdv {
-//     constructor(
-//         heureDebut,
-//         heureFin,
-//         nomPrenom,
-//         Assist,
-//         Presence,
-//         traite) {
-//     }
-// }
-
-// class Jour {
-//     constructor(dd, rdvs) {
-//     }
-// }
-
-// class Mois {
-//     constructor(mm, rdvs) {
-//     }
-// }
-
-// // DTO
-// function snapToApp(snap) {
-//     const jours = [];
-//     snap.map(snap => {
-//         // Créer les rdvs
-//         const rdvs = jour.rdv.map(rdv => {
-
-//         });
-//         // Créer un jour et Ajouter les rdv
-//         const jour = new Jour('', rdvs);
-//         // Ajouter le jour à la liste des jours
-//         jours.push(jour);
-//     });
-//     return jours;
-// }
+// Function à appeler par le front au chargement d'un type de permanence
+function getLatestPermanences(type) {
+    // console.log(type);
+    // 1. Récupérer la date (now)
+    const date = new Date();
+    const annee = date.getFullYear();
+    const mois = date.getMonth() + 1;
+    const jour = date.getDay();
+    // 2. Appeler le read avec (type, annee, mois en fonction de la date) et mettre le résultat dans une const
+    const data = read(type, annee, mois)
+        .then(rows => adapter(rows.val(), annee, mois));
+    return data;
+}
--- js/main.js
+++ js/main.js
@@ -1,27 +1,79 @@
-
 // let trashs = document.querySelectorAll('.trash');
-let trash = document.querySelector('.trash');
+// let trash = document.querySelector('.trash');
 // let containerToast = document.querySelector('.container-toast');
 let containerToast = document.getElementById('container-toast');
-
+let moisHere = document.querySelector('container-months');
+const tbody = document.getElementById('here-content-row');
 
 // trashs.forEach(trash => document.addEventListener('click',openModal()))
 
 // function openModal(){
 //   console.log('lol');
 // }
- trash.addEventListener('click', function() {
-  containerToast.style.display = "block";
- })
+// trash.addEventListener('click', function () {
+//     containerToast.style.display = "block";
+// })
 
 let dbRef = firebase.database().ref();
 
 // lancement de la function read 
-const data = read("PA", "2020", "08");
+// const data = read("PA", "2020", "08");
 
 //lancement de la function  update 
-const data1 = update("PA", "2020", "08", "19", "3", {
-  name: "cleoaaa",
-  presence: false,
-});
+// const data1 = update("PA", "2020", "08", "19", "3", {
+//   name: "cleoaaa",
+//   presence: false,
+// });
 
+function loadDataToDisplay() {
+    // 1. Appeler la fonction getLatestPermanences
+
+    const result = getLatestPermanences('PA');
+    result.then(annee => {
+        let ligne = '';
+
+        //   <td>
+        //     <input class="presence" type="checkbox" name="" id="presence">
+        //     <label for="presence">
+        //       <div class="tick_mark"></div>
+        //     </label>
+        //   </td>
+        //   <td><input class="presence" type="checkbox" name="" id="traite">
+        //     <label for="traite">
+        //       <div class="tick_mark"></div>
+        //     </label></td>
+
+        // moisHere.textContent = permanence;
+        // 2. Boucler sur la Structure pour injecter les données de façon dynamique dans l'HTML
+        console.log(annee);
+        annee.mois.forEach(m => {
+            m.jours.forEach(j => {
+                const date = `${j.date}/${m.mm}/${annee.yyyy}`;
+                j.rdvs.forEach((rdv, index) => {
+                    ligne += '<tr>';
+                    if (index === 0) {
+                        ligne += `<td rowspan='${j.rdvs.length}'>${date}</td>`;
+                    }
+                    ligne += `
+                          <td>${rdv.debut} à ${rdv.fin}</td>
+                          <td>${rdv.names}</td>
+                          <td>${rdv.tel}</td>
+                          <td>${rdv.ast}</td>
+                          <td>${rdv.presence}</td>
+                          <td>${rdv.traite}</td>
+                          <td class ='actions' id="${rdv.id}">
+                             <span class="pen"><i class="fas fa-pen"></i></span>
+                             <span class="check"><i class="fas fa-check"></i></span>
+                             <span class="trash"><i class="fas fa-trash"></i></span>
+                          </td>
+                          <td><textarea name="" id="note" cols="45"  rows="4">${rdv.commentaire}</textarea></td>`;
+                    ligne += '</tr>';
+                });
+            });
+        });
+        console.log(ligne);
+        tbody.innerHTML = ligne;
+    });
+}
+
+loadDataToDisplay();
